f0e0cb8a475bf0a95085718fdb2e9ddd8d4827ff
Laura <laura.rubio.arboleda1@correo.icesi.edu.co>
2018-09-15 12:08:34 -0500

    Clase Player

--- Lab2/src/mundo/Player.java
+++ Lab2/src/mundo/Player.java
@@ -1,5 +1,64 @@
 package mundo;
 
 public class Player {
+	
+	private String name;
+	private double ping;
+	private String geo;
+	private double skill;
+	private int id;
+	
+	public Player(String name, double ping, String geo, double skill, int id) {
+		
+		this.name=name;
+		this.ping=ping;
+		this.geo=geo;
+		this.skill=skill;
+		this.id=id;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPing() {
+		return ping;
+	}
+
+	public void setPing(double ping) {
+		this.ping = ping;
+	}
+
+	public String getGeo() {
+		return geo;
+	}
+
+	public void setGeo(String geo) {
+		this.geo = geo;
+	}
+
+	public double getSkill() {
+		return skill;
+	}
+
+	public void setSkill(double skill) {
+		this.skill = skill;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
 
 }
