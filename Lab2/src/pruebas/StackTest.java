d787f0ee8bdd44617190f4f768dde73f6c0e162d
Douglas <Douglas@172.30.146.64>
2018-09-19 15:57:15 -0500

    Pila para revisión

--- Lab2/src/pruebas/StackTest.java
+++ Lab2/src/pruebas/StackTest.java
@@ -15,10 +15,111 @@ public class StackTest {
 	}
 	@Test
 	public void testPush() {
-     String abc = (String) "Hola";
+     String abc = "Hola";
      relation.push(abc);
      
-     assertEquals(relation.getLast(), abc);
+     assertEquals(abc, relation.getLast().getValue());
+     
+     String abcd = "Pruebame este";
+     relation.push(abcd);
+     
+     assertEquals(abcd, relation.getLast().getValue());
+     
+     String abcde = "Pruebame este2";
+     relation.push(abcde);
+     
+     assertEquals(abcde, relation.getLast().getValue());
 	}
+	
+	@Test
+	public void testPop() {
+		 String abc = "Hola";
+	     relation.push(abc);
+	     
+	     String abcd = "Pruebame este";
+	     relation.push(abcd);
+	     
+	     String abcde = "Pruebame este2";
+	     relation.push(abcde);
+	     
+	     relation.pop();
+	     
+	     assertEquals(abcd, relation.getLast().getValue());
+	}
+	
+	@Test
+	public void testTop() {
+		
+		 String abc = "Hola";
+	     relation.push(abc);
+	     
+	     String abcd = "Pruebame este";
+	     relation.push(abcd);
+	     
+	     String abcde = "Pruebame este2";
+	     relation.push(abcde);
+	     
+	     assertEquals(abcde, relation.top());
+		
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		 String abc = "Hola";
+	     relation.push(abc);
+	     
+	     String abcd = "Pruebame este";
+	     relation.push(abcd);
+	     
+	     String abcde = "Pruebame este2";
+	     relation.push(abcde);
+	     
+	     boolean isEmpty = relation.isEmpty();
+	     
+	     assertTrue(false == isEmpty);
+		
+	}
+	
+	@Test
+	public void testSize() {
+		
+		 int contador = 3;
+		 
+		 String abc = "Hola";
+	     relation.push(abc);
+	     
+	     String abcd = "Pruebame este";
+	     relation.push(abcd);
+	     
+	     String abcde = "Pruebame este2";
+	     relation.push(abcde);
+	     
+	     int actual = relation.size();
+	     
+	     assertEquals(contador, actual);
+		
+	}
+	
+	@Test
+	public void testSearch() {
+		
+		 String abc = "Hola";
+	     relation.push(abc);
+	     
+	     String abcd = "Pruebame este";
+	     relation.push(abcd);
+	     
+	     String abcde = "Pruebame este2";
+	     relation.push(abcde);
+		
+		boolean actual = relation.search("Hola");
+		boolean hope = true;
+		
+		assertTrue(hope == actual);
+		
+	}
+	
+	
+	
 
 }
