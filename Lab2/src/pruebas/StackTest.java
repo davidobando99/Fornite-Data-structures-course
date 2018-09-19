29befdb889b84949bbc5477103f4ab3985496d7a
Douglas <Douglas@172.30.146.64>
2018-09-19 15:19:52 -0500

    JAJAJAJA CALLENSE

--- Lab2/src/pruebas/StackTest.java
+++ Lab2/src/pruebas/StackTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class StackTest<String> {
+public class StackTest {
  
 	private Stack<String> relation;
 	
